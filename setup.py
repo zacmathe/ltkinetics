0571742e936b94d2fa0c9bf8fd906719f64a4b60
Zachary Mathe <zacmathe@gmail.com>
2020-01-17 14:33:22 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ltkinetics',
+    version='0.2',
+    packages=find_packages(exclude=['tests*', 'examples*']),
+    license='GPL',
+    description='simulate nitrogenase kinetics',
+    long_description=open('README.md').read(),
+    install_requires=['numpy', 'scipy'],
+    url='https://github.com/zacmathe/ltkinetics',
+    author='Zachary Mathe',
+    author_email='zachary.mathe@cec.mpg.de',
+)
